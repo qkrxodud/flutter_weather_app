cf1d5d68a8dd207887c871904b346b9926a073bc
qkrxodud00 <qkrxodud00@gmail.com>
2022-02-18 21:32:21 +0900

    *로케이션 클래스 구성완료

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:flutter_weather_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -9,26 +9,21 @@ class LoadingScreen extends StatefulWidget {
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-  }
-
+class _LoadingScreenState extends State<LoadingScreen> {0
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getLocation();
   }
+
+  void getLocation() async {
+    Location location = Location();
+    location.getCurrentLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(onPressed:  () {
-          getLocation();
-        }, child: const Text("Get Location")),
-      ),
-    );
+    return Scaffold();
   }
 }
